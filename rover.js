378043b37ff2a62a765fbd3eecb2f36743da3000
dlm <dannyleematthew@gmail.com>
2021-08-22 01:04:24 -0400

    Began work on Rover

--- rover.js
+++ rover.js
@@ -3,3 +3,21 @@ class Rover {
 }
 
 module.exports = Rover;
+
+
+
+/*
+  >> Rover Class Description <<
+constructor(position)
+    position is a number representing the rover's position.
+    Sets this.position to position
+    Sets this.mode to 'NORMAL'
+    Sets default value for generatorWatts to 110
+receiveMessage(message)
+    message is a Message object
+    Returns an object containing at least two properties:
+        message: the name of the original Message object
+        results: an array of results. Each element in the array is an object that corresponds to one Command in message.commands.
+    Updates certain properties of the rover object
+        Details about how to respond to different commands are in the Command Types table.
+*/
